068bcae125ef3b098b2ac314812c78d053f00423
Damian <damiantopolskiit@gmail.com>
2019-11-24 21:57:33 +0100

    Added graph

--- src/main/java/pl/agh/edu/kis/sp2/sim/Application.java
+++ src/main/java/pl/agh/edu/kis/sp2/sim/Application.java
@@ -1,5 +1,7 @@
 package pl.agh.edu.kis.sp2.sim;
 
+import pl.agh.edu.kis.sp2.sim.generator.graph.WeightedGraph;
+
 public class Application {
 
     public static void main(String[] args) {
@@ -8,5 +10,18 @@ public class Application {
 	    sim.showRandValues();
 	    sim.showRandValues();
 	    sim.showRandValues();
+
+		int vertices = 6;
+		WeightedGraph.Graph graph = new WeightedGraph.Graph(vertices);
+		graph.addEgde(0, 1, 4);
+		graph.addEgde(0, 2, 3);
+		graph.addEgde(1, 3, 2);
+		graph.addEgde(1, 2, 5);
+		graph.addEgde(2, 3, 7);
+		graph.addEgde(3, 4, 2);
+		graph.addEgde(4, 0, 4);
+		graph.addEgde(4, 1, 4);
+		graph.addEgde(4, 5, 6);
+		graph.printGraph();
     }
 }
